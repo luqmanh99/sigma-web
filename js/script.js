78b248f26ac9713192734723ffdd135eff67c436
man <man@gmail.comgit config --global user.name  man;git config --global user.email man@gmail.com>
2023-04-17 11:13:45 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+ 
+// var swiper = new Swiper(".slide-content", {
+//     slidesPerView: 3,
+//     spaceBetween: 25,
+//     loop: true,
+//     centerSlide: 'true',
+//     fade: 'true',
+//     grabCursor: 'true',
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//       dynamicBullets: true,
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+
+//     breakpoints:{
+//         0: {
+//             slidesPerView: 1,
+//         },
+//         520: {
+//             slidesPerView: 2,
+//         },
+//         950: {
+//             slidesPerView: 3,
+//         },
+//     },
+//   });
+
+
+  let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click", function () {
+    navbar.classList.toggle("active");
+});
+
+window.onscroll = () => {
+    navbar.classList.remove("active");
+}
